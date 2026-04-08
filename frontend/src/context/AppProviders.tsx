c4a69c933ad9deaa4e841314aeae7f8f53c64f3d
Eugene Russkikh <z8a7hp@gmail.com>
2026-04-08 05:42:27 +0200

    chore: remove dead code (UIContext, OverviewPanel, ResizeHandle)

--- frontend/src/context/AppProviders.tsx
+++ frontend/src/context/AppProviders.tsx
@@ -3,18 +3,14 @@ import { AppProvider } from './AppContext';
 import { GraphProvider } from './GraphContext';
 import { KGProvider } from './KGContext';
 import { ThemeProvider } from './ThemeContext';
-import { UIProvider } from './UIContext';
-
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider>
-      <UIProvider>
-        <KGProvider>
-          <GraphProvider>
-            <AppProvider>{children}</AppProvider>
-          </GraphProvider>
-        </KGProvider>
-      </UIProvider>
+      <KGProvider>
+        <GraphProvider>
+          <AppProvider>{children}</AppProvider>
+        </GraphProvider>
+      </KGProvider>
     </ThemeProvider>
   );
 }
