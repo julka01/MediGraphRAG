ae8ecb04ff414e9bdec418ab44261a1718a8b65b
Eugene Russkikh <z8a7hp@gmail.com>
2026-04-13 23:24:38 +0200

    fix: restore px-1 on fieldset legends for compact sizing

--- frontend/src/components/kg/KGBuildSection.tsx
+++ frontend/src/components/kg/KGBuildSection.tsx
@@ -216,7 +216,7 @@ export function KGBuildSection({
 
       {/* Max Chunks */}
       <fieldset className="fieldset border rounded-lg px-3 pb-2 pt-0 border-base-content/20 focus-within:border-primary/50 transition-colors">
-        <legend className="fieldset-legend text-2xs text-base-content/50 ml-auto mr-2">Max Chunks</legend>
+        <legend className="fieldset-legend text-2xs text-base-content/50 px-1 ml-auto mr-2">Max Chunks</legend>
         <input
           type="number"
           name="max-chunks"
@@ -231,7 +231,7 @@ export function KGBuildSection({
 
       {/* KG Name */}
       <fieldset className="fieldset border rounded-lg px-3 pb-2 pt-0 border-base-content/20 focus-within:border-primary/50 transition-colors">
-        <legend className="fieldset-legend text-2xs text-base-content/50 ml-auto mr-2">KG Name</legend>
+        <legend className="fieldset-legend text-2xs text-base-content/50 px-1 ml-auto mr-2">KG Name</legend>
         <input
           type="text"
           name="kg-name"
